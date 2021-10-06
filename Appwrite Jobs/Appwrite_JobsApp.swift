941283342348ca42145eab783131c2a888cf8810
Damodar Lohani <dlohani48@gmail.com>
2021-10-06 12:36:45 +0545

    Initial Commit

---
+++ Appwrite Jobs/Appwrite_JobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appwrite_JobsApp.swift
+//  Appwrite Jobs
+//
+//  Created by Damodar Lohani on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Appwrite_JobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
